f7f62de01ec5e601456f1b30261859cb7b5becc9
imikodanic <imikodanic@gmail.com>
2024-07-24 19:01:31 +0200

    feat: trying to integrate treblle

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { useNestTreblle } from 'treblle';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,5 +8,13 @@ async function bootstrap() {
     origin: true,
   });
   await app.listen(3000);
+
+  const expressInstance = app.getHttpAdapter().getInstance();
+
+  useNestTreblle(expressInstance, {
+    apiKey: 'Zp1Enon4g4BR3ZpIBYzGyfQGsdVnzxsXNuZM6bFyZestWvSQPEnU8tQ49rplalQh',
+    projectId: '01j3jsp7c86hbk1axjsbzxb2wp',
+  });
 }
+
 bootstrap();
